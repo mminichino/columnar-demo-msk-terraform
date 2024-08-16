3740b40416bc02a461ba5c60bea4a64f094f3ef4
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-08-16 12:35:31 -0500

    Version 1.0.0

--- modules/configure/main.tf
+++ modules/configure/main.tf
@@ -13,7 +13,7 @@ data "aws_msk_configuration" "cluster" {
 
 resource "terraform_data" "enable_public_access" {
   triggers_replace = [
-    var.msk_cluster_id
+    var.msk_cluster_version
   ]
   provisioner "local-exec" {
     command = "aws kafka update-connectivity --cluster-arn ${var.msk_cluster_arn} --current-version ${var.msk_cluster_version} --connectivity-info '{\"PublicAccess\": {\"Type\": \"SERVICE_PROVIDED_EIPS\"}}' --region ${var.region}"
